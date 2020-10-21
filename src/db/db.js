b6d8a242e1700d5c7ed15822280ed28bef0ea976
Josh Dhudwal <joshdhudwal@Joshs-MacBook-Pro.local>
2020-10-20 22:28:29 -0700

    database connection test

---
+++ src/db/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const config = require("config");
+
+const db = config.get('mongoURI');
+
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiesTopology: true
+        });
+        console.log("mongoose connected");
+    }catch(err){
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+
+
+module.exports = connectDB;
--- src/express.js
+++ src/express.js
@@ -1,15 +1,24 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 2000;
+const connectDB = require("./db/db");
+
 export default app;
 
 export const initExpress = async () => {
+
+    //connect to DB test
+    connectDB();
+   
+
     app.get('/', (req, res) => {
         res.send('Testing Git on jd branch')
     });
 
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}!`)
+    app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}!`)
     });
+
+
 }
