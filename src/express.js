859b584b9b79aaec6387806b9b5ad514472bb044
Josh Dhudwal <joshdhudwal@Joshs-MacBook-Pro.local>
2020-11-02 21:43:28 -0800

    initial login and registration to database

--- src/express.js
+++ src/express.js
@@ -7,54 +7,20 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require("./db/db");
 const mongoose = require("mongoose");
 const User = require("./models/User");
-//const Account = require("./models/Account");
+
 
 export default app
 
 export const initExpress = async () => {
-
-    //connect to DB test
-    connectDB();
+    
    
     app.use(bodyParser.json({ limit: '100mb' }));
     app.use(bodyParser.raw());
     app.use(bodyParser.urlencoded({ extended: false }));
 
+    connectDB();
     app.use('/api', routes);
 
-    //initial database entry tests
-    app.post('/', (req,res) => {
-        console.log("Incoming request body: ", req.body);
-        console.log("accounts: ", req.body.accounts);
-        
-        let newUser = new User({
-            username: req.body.username,
-            password: req.body.password,
-            pin: req.body.pin,
-            transactions: req.body.transactions,
-            accounts: req.body.accounts
-        });
-
-        console.log("about to create new user: ", newUser);
-        //console.log("newUser.username: ", newUser.username);
-        try{
-            
-            newUser.save(function(err){
-                if(err){
-                    console.log("Error creating user",err);
-                }else{
-                    console.log("Created User");
-                }
-            });
-            console.log("User Created");
-
-        }catch(error){
-            console.log("error");
-
-        }
-
-    });
-  
     app.listen(PORT, () => {
         console.log(`Bank app backend listening on port ${PORT}!`)
     });
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,23 +1,24 @@
 import uuid from 'uuid';
+import User from '../models/User';
 
 export const login = async (username, password) => {
     // compare password
-    const exampleuser = {
+    let loggedUser = new User({
         id: 1,
-        username,
-        password,
-        pin: 1234
-    }
-    return exampleuser;
+        username:username,
+        password:password,
+        //pin: 1234
+    });
+    
+    return loggedUser;
 }
 
 export const register = async (username, password, pin) => {
     // create new user
-    const exampleuser = {
-        id: uuid(),
-        username,
-        password,
-        pin
-    }
-    return exampleuser;
+    let newUser = new User({
+        username: username,
+        password: password,
+        pin: pin
+    });
+    return newUser;
 }
